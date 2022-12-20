eafb4aec02d9cac8f515a3152a8f95ffbe9e9bbb
musaubrian <musaubrian15@gmail.com>
2022-12-20 22:36:27 +0300

    chore: create pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,4 +5,24 @@ return a list of lists of integers representing the Pascal’s triangle of n
 
 
 def pascal_triangle(n):
-    pass
+    """
+    return pascal's triangle
+
+Args::
+    n(int): the starting point of the triangle
+    """
+
+    triangle = []
+
+    if n <= 0:
+        return triangle
+    else:
+        for i in range(n):
+            row = []
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            triangle.append(row)
+        return triangle
