b94a91e79b83b29627bb5365c8fb4620cae63437
musaubrian <musaubrian15@gmail.com>
2022-12-21 15:50:11 +0300

    chore: reduce nested loops

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,19 +12,19 @@ def pascal_triangle(n):
         n(int): the starting point of the triangle
     """
 
-    triangle = []
 
     if type(n) is not int:
         raise TypeError(f'[{n}] is not of type int\nUse an int value')
-    elif n <= 0:
-        return triangle
-    else:
-        for i in range(n):
-            row = []
-            for j in range(i+1):
-                if j == 0 or j == i:
-                    row.append(1)
-                else:
-                    row.append(triangle[i-1][j-1] + triangle[i-1][j])
-            triangle.append(row)
-        return triangle
+    if n <= 0:
+        return []
+    
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(row)
+    return triangle
