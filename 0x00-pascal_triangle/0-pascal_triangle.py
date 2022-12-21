0f1bb2fd77a0770eb014d4e0b350cc759122b286
musaubrian <musaubrian15@gmail.com>
2022-12-21 16:10:26 +0300

    chore: match codestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,5 +18,5 @@ def pascal_triangle(n):
     triangle.append([1])
     for i in range(n - 1):
         triangle.append([1] + [triangle[i][j] + triangle[i][j + 1]
-                    for j in range(len(triangle[i]) - 1)] + [1])
+                               for j in range(len(triangle[i]) - 1)] + [1])
     return triangle
