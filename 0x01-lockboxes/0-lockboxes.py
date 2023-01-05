9a1be2eed1a13a546b578052c7639f2261960db3
musaubrian <musaubrian15@gmail.com>
2023-01-05 23:56:20 +0300

    chore: solve boxes task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,4 +3,18 @@
 
 
 def canUnlockAll(boxes):
-    pass
+    """
+    determines if all the boxes can be opened
+    Args::
+        boxes(list): list of integer lists
+    """
+    visited_boxes = set()
+
+    queue = [0]
+
+    while queue:
+        box = queue.pop(0)
+        if box not in visited_boxes:
+            visited_boxes.add(box)
+            queue += boxes[box]
+    return len(visited_boxes) == len(boxes)
