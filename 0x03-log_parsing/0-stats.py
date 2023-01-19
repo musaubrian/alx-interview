6926daa51462982d0deefea5f56c3e9554d73a73
Ernest <musaubrian15@gmail.com>
2023-01-19 17:11:21 +0300

    chore: update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,42 @@
 #!/usr/bin/python3
-"""
-reads stdin line by line and computes metrics
-"""
+"""Python script that reads stdin line by line and computes metrics"""
+
 import sys
-from typing import Tuple
 
 
-def compute_metrics() -> Tuple[int, dict[int, int]]:
-    """"""
-    total = 0
-    status_count = {
-            200: 0,
-            301: 0,
-            400: 0,
-            401: 0,
-            403: 0,
-            404: 0,
-            405: 0,
-            500: 0
-            }
-    line_count = 0
+def print_n(t_file_size, status):
+    """Prints total file size and status list"""
+    print("File size: {:d}".format(t_file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
+
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
+
+t_file_size = 0
+count = 0
+try:
     for line in sys.stdin:
-        line_count += 1
-        parts = line.split(" ")
+        args = line.split()
 
-        if len(parts) != 9:
-            continue
-        
-        ip, date, request, status, size = parts[0], parts[3], parts[5],\
-                parts[7], parts[8]
+        if len(args) > 2:
+            status_code = args[-2]
+            file_size = int(args[-1])
 
-        status = int(status)
-        size = int(size)
+            if status_code in status:
+                status[status_code] += 1
 
-        if status in status_count:
-            status_count[status] += 2
+            t_file_size += file_size
+            count += 1
 
-        total += size
+            if count == 10:
+                print_n(t_file_size, status)
+                count = 0
 
-        if line_count % 10 == 0:
-            print(f"Total file size: {total}")
-            for status in sorted(status_count):
-                if status_count[status] > 0:
-                    print(f"{status}: {status_count[status]}")
+except KeyboardInterrupt:
+    pass
 
-        return total, status_count
-
-
-if __name__ == "__main__":
-    try:
-        compute_metrics()
-    except KeyboardInterrupt:
-        total, status_count = compute_metrics()
-        print(f"Total file size: {total}")
-        for status in sorted(status_count):
-            if status_count > 0:
-                print(f"{status}: {status_count[status]}")
+finally:
+    print_n(t_file_size, status)
