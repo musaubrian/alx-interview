322791b3177f9bf31055b96808032bc168434d2b
Ernest <musaubrian15@gmail.com>
2023-02-16 11:52:13 +0300

    chore: rotate a matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,12 @@
 Rotates a 2D array by 90 degrees clockwise
 """
 
+
 def rotate_2d_matrix(matrix):
     """rotate a 2D array 90deg clockwise in place"""
-    print(matrix)
+    matrix_len = len(matrix)
+    matrix_copy = [[x for x in row] for row in matrix]
+
+    for i in range(0, matrix_len):
+        for j in range(0, matrix_len):
+            matrix[j][matrix_len - 1 - i] = matrix_copy[i][j]
