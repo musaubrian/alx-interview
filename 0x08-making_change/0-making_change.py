6d53847f3912e7cc6821c7a2101c444ef77f8b9c
Ernest <musaubrian15@gmail.com>
2023-02-26 14:17:55 +0300

    chore: solution to make change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module returns the fewest number of coins
+needed to meet a given amount total. 
+"""
+
+
+def makeChange(coins, total) -> int:
+    """
+    Returns the fewest number of coins
+    needed to meet a given amount total.
+    """
+
+    if total <= 0:
+        return 0
+
+    number_of_coins = 0
+    cents = 0
+
+    coins = sorted(coins, reverse=True)
+
+    for coin in coins:
+        while cents + coin <= total:
+            cents += coin
+            number_of_coins += 1
+        if cents == total:
+            return number_of_coins
+    return -1
